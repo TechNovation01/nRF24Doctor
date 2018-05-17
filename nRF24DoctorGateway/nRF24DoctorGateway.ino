85af2f3c9415b1f042e74a2f3ffce3b10d67040d
TechNovation01 <TechNovation@live.nl>
2018-05-17 21:25:44 +0200

    Get&Set MessageRate
    Added functionality and menu items to set the Msg/sec and check the actual Msg/sec.

--- nRF24DoctorGateway/nRF24DoctorGateway.ino
+++ nRF24DoctorGateway/nRF24DoctorGateway.ino
@@ -45,6 +45,8 @@
 //#define MY_RADIO_NRF5_ESB
 //#define MY_RADIO_RFM69
 //#define MY_RADIO_RFM95
+#define MY_RF24_IRQ_PIN (2)
+#define MY_RX_MESSAGE_BUFFER_FEATURE
 
 // Set LOW transmit power level as default, if you have an amplified NRF-module and
 // power your radio separately with a good regulator you can turn up PA level.
